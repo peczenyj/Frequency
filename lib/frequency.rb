f6f2ed0cadd20351a7a5e07ec601bdc3676ca25c
Tiago Peczenyj <tiago.peczenyj@gmail.com>
2010-05-19 17:55:47 -0300

    deixando profissa

--- lib/frequency.rb
+++ lib/frequency.rb
@@ -29,25 +29,3 @@ module Frequency
     yield if rand < rate
   end
 end
-
-puts "testes"
-
-x = { :custom => 0, :never => 0, :sometimes => 0, :rarely => 0, :normally => 0, :always =>0 }
-
-Max = 10000
-Max.times do |i|
-  Frequency.always    { x[:always]   += 1 }
-  Frequency.normally  { x[:normally] += 1 }
-  Frequency.sometimes { x[:sometimes]+= 1 }
-  Frequency.rarely    { x[:rarely]   += 1 } 
-  Frequency.never     { x[:never]    += 1 }
-  
-  Frequency.sometimes :with_probability => 0.12 do
-    x[:custom] += 1
-  end
-end
-
-x.each do |i,j|
-  puts "#{i} => #{j*100.0/Max}%"
-end
-
