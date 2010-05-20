522e14f578fa928f38627811cd433d14a33bd2d4
Tiago Peczenyj <tiago.peczenyj@gmail.com>
2010-05-19 22:03:41 -0300

    add rdoc

--- lib/frequency.rb
+++ lib/frequency.rb
@@ -1,24 +1,52 @@
+# Module Frequency
+# A small dsl written in ruby to work with frequency events (never, sometimes, always..)
+#
 module Frequency
   NORMALLY  = 0.75
   SOMETIMES = 0.50
   RARELY    = 0.25
   
+  # always do something
+  # example:
+  #   always { puts "ok"}
   def always(cond={})
     yield if block_given?
   end
   
+  # normally (75%) do something,
+  # see sometimes method
+  # example:
+  #   normally { puts "ok"}
   def normally(cond={})
     execute_with_probability(cond,NORMALLY ) { yield } if block_given?
   end
   
+  # sometimes (50%) do something
+  # example:
+  #   sometimes do
+  #      # some code
+  #   end
+  # or adjusting the probability
+  #   sometimes :with_probability => 0.12 do
+  #     # some code
+  #   end
+  # you can use "12%" instead 0.12
+  #   sometimes(:with_probability => '13.6%') { ... }
   def sometimes(cond={})
     execute_with_probability(cond,SOMETIMES) { yield } if block_given?
   end
   
+  # rarely (25%) do something,
+  # see sometimes method
+  # example:
+  #   rarely { puts "ok"}
   def rarely(cond={})
     execute_with_probability(cond,RARELY   ) { yield } if block_given?
   end
   
+  # never do something
+  # example:
+  #   never { puts "ok"}  
   def never(cond={})
     nil
   end
